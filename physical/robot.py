88a94b9ca330f1c260aa5093ec11f0c094768100
Zac Nolan <zac1i1nolan@gmail.com>
2023-01-24 16:36:57 +0000

    cleaned code

--- physical/robot.py
+++ physical/robot.py
@@ -3,7 +3,7 @@ from sr.robot3 import *
 #import time
 from sr.robot3 import *
 R = Robot()
-print("Hello worldssss")
+
 #while True:
 R.kch.leds[UserLED.A] = Colour.RED
     #R.kch.leds[UserLED.A] = Colour.OFF
@@ -11,13 +11,8 @@ R.kch.leds[UserLED.A] = Colour.RED
     
 while True:
     #motor board srABC1, channel 0 to full power forward
-    print("about to the motor i thin")
-
     R.motor_boards["SR0UDB"].motors[0].power = 1
     R.motor_boards["SR0UDB"].motors[1].power = 1
-    print("done the motor")
-
-
     # motor board srABC1, channel 1 to half power forward
         #R.motor_boards["SR0UDB"].motors[1].power = -0.2
         
