0a3fdf5a4579e70ccadc3ff62ab71cd21483ff80
Zac Nolan <zac1i1nolan@gmail.com>
2023-01-31 17:31:56 +0000

    updated log and robot

--- physical/robot.py
+++ physical/robot.py
@@ -2,36 +2,82 @@ from sr.robot3 import *
 R = Robot()
 
 
-R.kch.leds[UserLED.A] = Colour.RED
+def task_2():
 
-left_id = 1
-mid_id = 2
-right_id = 3
+    left_id = 1
+    mid_id = 2
+    right_id = 3
+        
+    lit_up = False
     
-lit_up = False
-
-
-while True:
+    print("set up variables")
     
-    markers = R.camera.see() # markers
+    while True:
+        
+        print("loop start")
+        markers = R.camera.see() # markers
+        print("got markers")
+
+        for each in markers:
+            print("finding mid")
+            if each.id == mid_id:
+                print("found mid")
+                R.kch.leds[UserLED.B] = Colour.BLUE # B
+                lit_up = True
+
+
+        if not lit_up:
+            print("if can see either of the other markes")
+            for each in markers:
+                if each.id == left_id:
+                    print("saw left marker")
+                    R.kch.leds[UserLED.A] = Colour.BLUE # A
+                    
+                    lit_up = True
+                
+                elif each.id == right_id:
+                    print("saw right marker")
+                    R.kch.leds[UserLED.C] = Colour.BLUE # C
+            
+        
+        lit_up = False
+            
+        R.sleep(0.5) 
+
+        R.kch.leds[UserLED.A] = Colour.OFF
+        R.kch.leds[UserLED.B] = Colour.OFF
+        R.kch.leds[UserLED.C] = Colour.OFF
+        #motor board srABC1, channel 0 to full power forward
+        #R.motor_boards["SR0UDB"].motors[0].power = 1
+        #R.motor_boards["SR0UDB"].motors[1].power = 1
+
+
+def task_3():
+
+    mid_id = 2
+        
+    markers = R.camera.see()
 
     for each in markers:
         if each.id == mid_id:
-            R.khc.leds[UserLED.B] = Colour.BLUE # B
-            lit_up = True
+            d = each.distance
+            break
 
-    if not lit_up:
-        for each in markers:
-            if each.id == left_id:
-                R.khc.leds[UserLED.A] = Colour.BLUE # A
-                lit_up = True
-            
-            elif each.id == right_id:
-                R.khc.leds[UserLED.C] = Colour.BLUE # C
+    d /= 1000
+       
+    dist = ((d**2 - 1)**0.5) * 1000
+    
+    if dist <= 200:
+        R.kch.leds[UserLED.B] = Colour.BLUE
+
+                
+    else:
+                
+        if each.spherical.rot_y > 0:
+            R.kch.leds[UserLED.A] = Colour.RED
+        else:
+            R.kch.leds[UserLED.C] = Colour.RED
+             
         
     
-    lit_up = False
-    R.sleep(0.1) 
-    #motor board srABC1, channel 0 to full power forward
-    #R.motor_boards["SR0UDB"].motors[0].power = 1
-    #R.motor_boards["SR0UDB"].motors[1].power = 1
+task_2()
